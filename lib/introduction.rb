450c0ae0f18e459b4b3c64992ff85dc4ed4ff043
Bryant Omoregie <unclebryant@gmail.com>
2020-02-06 01:55:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,8 @@ def introduction_with_language(name, language)
 end
 
 def introduction_with_language_optional(name, language=ruby)
-  puts ""
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
 
 
 
